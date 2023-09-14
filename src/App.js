85f8c3c6810fb9f3fe548b1317f78377213a3878
WooMiDo <kjk2875@bizspring.co.kr>
2023-09-14 13:51:53 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import DashBoardPage from "./page/DashBoardPage";
 import { SendDataContextProvider } from "./context/sendDataContext";
 import { RouterProvider } from "react-router-dom";
 import routers from "./routers";
-import Practice from "./layout/Practice";
 
 function App() {
   return (
